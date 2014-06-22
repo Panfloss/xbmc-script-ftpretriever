4b2af8e85e10b2d6618296337d09bc5933d51b00
panfloss <gallarusdebrb@gmail.com>
2014-06-22 20:02:14 +0200

    add a progressbarBG during the process

--- addon.py
+++ addon.py
@@ -1,6 +1,7 @@
 import resources.lib.sync_ftp as sync_ftp
 import xbmc
 import xbmcaddon
+import xbmcgui
 import os
 import json
 
@@ -10,13 +11,16 @@ __icon__        = __addon__.getAddonInfo('icon')
 __profile__ = xbmc.translatePath(__addon__.getAddonInfo('profile') ).decode("utf-8")
 
 
+pDialog = xbmcgui.DialogProgressBG()
+pDialog.create('FTP retriever', 'Accessing credentials')
+
 host = __addon__.getSetting("host")
 user = __addon__.getSetting("username")
 passwd = __addon__.getSetting("password")
 local_folder = __addon__.getSetting("local_folder")
 distant_folder = __addon__.getSetting("distant_folder")
 
-xbmc.executebuiltin('Notification(%s, %s, %d, %s)'%(__addonname__,"FTP retrieving is starting", 5000, __icon__))
+pDialog.update(5, message='Recovering list of up to date files')
 
 try:
     os.chdir(__profile__)
@@ -25,13 +29,18 @@ try:
 except:
     ignore_list = ""
 
+
+pDialog.update(10, message='File retrieving in progress...')
+
 ftpInstance = sync_ftp.FtpSession(host, user, passwd)
 
 ignore_list = ftpInstance.sync_folder(local_folder, distant_folder, ignore_list)
+
+pDialog.update(95, message='Updating list of up to date files')
 
 os.chdir(__profile__)
 with open("ignore_list.json", "w") as file:
     json.dump(ignore_list, file, sort_keys=True, indent=4, separators=(',', ': '))
 
-
-xbmc.executebuiltin('Notification(%s, %s, %d, %s)'%(__addonname__,"FTP retrieving is finished", 5000, __icon__))
+pDialog.update(100, message=' ')
+pDialog.close()
--- tasklist.md
+++ tasklist.md
@@ -1,7 +1,6 @@
 Task List
 =========
 
-- [ ] use a progress bar to show when the addon is working (dialogprogressBG)
 - [ ] allow to sync to a smb:// or other special mount point with xbmc.cvfs
 - [ ] prevent script from crashing if a file or a folder is not accessible
 - [ ] prevent script from crashing if a folder is already synced (and check if everything in it is rightly synced : after crash recovery)
@@ -9,3 +8,4 @@ Task List
 - [x] add a notification when the download start and when it's over
 - [x] make ignore_list an argument of sync folder and store it with xbmc addon_data
 - [x] reorganise ftp_sync : it should allow the creation of an object that represent the ftp session and have method to sync folder
+- [X] use a progress bar to show when the addon is working (dialogProgressBG)
