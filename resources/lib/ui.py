5b2176f738a9fc5a4f973e98a18ceebc8e5c1024
gallarus <gallarusdebrb@gmail.com>
2014-09-24 16:57:38 +0000

    error correction

--- resources/lib/ui.py
+++ resources/lib/ui.py
@@ -23,13 +23,13 @@ class SyncProgressBarBG(object):
     def update_file_dl(self, file, tot_files, file_number):
         "specialized update fn for when downloading file"
         
-        text = self.language(32001).format(file_number, tot_files, file))
+        text = self.language(32001).format(file_number, tot_files, file)
         
         #resize "name" if it does not fit in the progress bar (50 chars wide)
         excess = len(text) - 50
         if excess > 0:
-            file = file[:len(file)*2/3 - (excess/2)] + file[len(file)*2/3 + (excess/2)+1:]
-            text = self.language(32001).format(file_number, tot_files, file))
+            file = file[:len(file)*2/3 - (excess/2)] + " ... "  + file[len(file)*2/3 + (excess/2)+6:]
+            text = self.language(32001).format(file_number, tot_files, file)
         
         self._pDialog.update(file_number*100 / tot_files, message=text)
 
