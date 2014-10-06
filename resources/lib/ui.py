ef344ba2f3fb932e7a72a740038d92651066a4f0
gallarus <gallarusdebrb@gmail.com>
2014-10-06 15:32:23 +0000

    begining debug

--- resources/lib/ui.py
+++ resources/lib/ui.py
@@ -2,6 +2,7 @@ import xbmcgui
 import sys
 
 errors = []
+language = None
 
 class SyncProgressBarBG(object):
     "class to handle the background progressbar"
@@ -12,11 +13,11 @@ class SyncProgressBarBG(object):
     def __init__(self, heading):
         "create the DialogProgressBG "
 
-        self.language = sys.modules["__main__"].language
+        language = sys.modules["__main__"].language
         self._addon_name = heading
 
         self._pDialog = xbmcgui.DialogProgressBG()
-        self._pDialog.create(heading, self.language(32000))
+        self._pDialog.create(heading, language(32000))
 
     def update(self, percent, msg):
         "wrap the update function"
@@ -26,14 +27,14 @@ class SyncProgressBarBG(object):
     def update_file_dl(self, file, tot_files, file_number):
         "specialized update fn for when downloading file"
 
-        text = self.language(32001).format(file_number, tot_files, file)
+        text = language(32001).format(file_number, tot_files, file)
 
         self._pDialog.update(file_number*100 / tot_files, message=text)
 
     def update_profile(self, tot_profile, profile_number):
         "specialized update fn for when changing profile"
 
-        self._pDialog.update(profile_number*100 / tot_profile , heading=self._addon_name + " " + self.language(32002).format(profile_number, tot_profile), message=self.language(32000))
+        self._pDialog.update(profile_number*100 / tot_profile , heading=self._addon_name + " " + language(32002).format(profile_number, tot_profile), message=language(32000))
 
     def close(self):
         """
@@ -47,15 +48,13 @@ def ftpConnectionError(profile_number, error):
     """
     Function adding connection errors to the list of errors
     """
-    language = sys.modules["__main__"].language
     errors.append(language(33001).format(profile_number, error))
-    
+
 def notifyErrors():
     """
     Pop an ok dialog box if some errors occured
     """
-    
+
     if errors != []:
         message = "\n".join(errors)
         xbmcgui.Dialog().ok("XBMC FTP Retriever", language(33000) + message)
-        
