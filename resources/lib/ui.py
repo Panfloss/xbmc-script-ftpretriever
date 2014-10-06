0788db56e7c83dce48fdd899b5331dec461c4cb7
gallarus <gallarusdebrb@gmail.com>
2014-10-06 12:44:03 +0000

    begining debug

--- resources/lib/ui.py
+++ resources/lib/ui.py
@@ -1,7 +1,6 @@
 import xbmcgui
 import sys
 
-language = sys.modules["__main__"].language
 
 class SyncProgressBarBG(object):
     "class to handle the background progressbar"
@@ -12,10 +11,11 @@ class SyncProgressBarBG(object):
     def __init__(self, heading):
         "create the DialogProgressBG "
 
+        self.language = sys.modules["__main__"].language
         self._addon_name = heading
-        
+
         self._pDialog = xbmcgui.DialogProgressBG()
-        self._pDialog.create(heading, language(32000))
+        self._pDialog.create(heading, self.language(32000))
 
     def update(self, percent, msg):
         "wrap the update function"
@@ -24,15 +24,15 @@ class SyncProgressBarBG(object):
 
     def update_file_dl(self, file, tot_files, file_number):
         "specialized update fn for when downloading file"
-        
-        text = language(32001).format(file_number, tot_files, file)
-        
+
+        text = self.language(32001).format(file_number, tot_files, file)
+
         self._pDialog.update(file_number*100 / tot_files, message=text)
 
     def update_profile(self, tot_profile, profile_number):
         "specialized update fn for when changing profile"
 
-        self._pDialog.update(profile_number*100 / tot_profile , heading=self._addon_name + " " + language(32002).format(profile_number, tot_profile), message=language(32000))
+        self._pDialog.update(profile_number*100 / tot_profile , heading=self._addon_name + " " + self.language(32002).format(profile_number, tot_profile), message=self.language(32000))
 
     def close(self):
         """
@@ -46,5 +46,5 @@ def ftpConnectionError(profile_number, error):
     """
     Function handling the notification of a connection error
     """
-    
-    xbmcgui.Dialog().ok("XBMC FTP Retriever", language(33001).format(profile_number, error))
+    language = sys.modules["__main__"].language
+    xbmcgui.Dialog().ok("XBMC FTP Retriever", language(32002).format(profile_numbere))
