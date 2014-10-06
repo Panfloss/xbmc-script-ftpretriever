fc66cb6eb663af26d8ba7ec7697ddfef0aa1d9f0
gallarus <gallarusdebrb@gmail.com>
2014-10-06 12:15:50 +0000

    begining debug

--- resources/lib/ftp.py
+++ resources/lib/ftp.py
@@ -33,6 +33,9 @@ class FtpSession(object):
         except ftplib.error_perm: # how to catch ftp errors
             xbmcgui.Dialog().ok("an error occured", "Username or password incorect")
             return False
+        except ftplib.gaierror: # how to catch ftp errors
+            xbmcgui.Dialog().ok("an error occured", "Hostname incorect")
+            return False
         
         return True
 
